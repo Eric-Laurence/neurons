271b3b0b3e4cbddc15df4958e8fef70f68d19a77
Eric Laurence <eric@laurences.net>
2018-07-16 10:56:12 -0700

    Add Neuron class

--- neuron.py
+++ neuron.py
@@ -1,21 +1,33 @@
 a = 2
 b = 4
 
-def weightedSum (x, y):
-    return a*x + b*y
 
-def activate (x):
-    if x > 0.5:
-        return 1
-    else:
-        return 0
+class Neuron:
     
-def output (x, y):
-    return activate(weightedSum(x, y)) 
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
     
+
+    def weightedSum (self, x, y):
+        return self.a*x + self.b*y
+
+    def activate (self, x):
+        if x > 0.5:
+            return 1
+        else:
+            return 0
+    
+    def output (self, x, y):
+        return self.activate(self.weightedSum(x, y)) 
+
+
+neuron1 = Neuron(a, b)
+
+
 #print("%d" % weightedSum(5, 3))
-print(weightedSum(5, 3)) 
-print(activate(weightedSum(5, 3)))
-print(output(5, 3))
+print(neuron1.weightedSum(5, 3)) 
+print(neuron1.activate(neuron1.weightedSum(5, 3)))
+print(neuron1.output(5, 3))
 
     

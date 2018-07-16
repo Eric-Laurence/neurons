86bcab96108ab4b947ea3788448445c5535ce126
Eric Laurence <eric@laurences.net>
2018-07-16 10:59:12 -0700

    Variable numbers of coefficients

--- neuron.py
+++ neuron.py
@@ -1,12 +1,22 @@
+#!/usr/bin/python3
+
+class ArgumentError (Exception):
+    pass
+
 class Neuron:
     
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def __init__(self, *coefficients):
+        self.coefficients = coefficients
     
-
-    def weightedSum (self, x, y):
-        return self.a*x + self.b*y
+    def weightedSum (self, *inputs):
+        if len(inputs) != len(self.coefficients):
+            raise ArgumentError
+        
+        sum = 0
+        for i in range(len(inputs)):
+            sum += inputs[i] * self.coefficients[i]
+        
+        return sum
 
     def activate (self, x):
         if x > 0.5:
@@ -14,8 +24,8 @@ class Neuron:
         else:
             return 0
     
-    def output (self, x, y):
-        return self.activate(self.weightedSum(x, y)) 
+    def output (self, *inputs):
+        return self.activate(self.weightedSum(*inputs)) 
 
 input_list = [
     (5, 3),
@@ -33,15 +43,23 @@ coefficient_list = [
 
 neuron_list = []
 
-for (a, b) in coefficient_list:
+for a, b in coefficient_list:
     neuron_list.append(Neuron(a, b))
     
     
 for neuron_i, neuron in enumerate(neuron_list):
+    print()
     print("neuron " + str(neuron_i + 1))
-    for (x, y) in input_list:
-        print("    " + str(neuron.weightedSum(x, y)))
-        print("    " + str(neuron.output(x, y)))
+    for x, y in input_list:
+        print("    weighted sum: " + str(neuron.weightedSum(x, y)))
+        print("    output: " + str(neuron.output(x, y)))
+        
+
+print()
+print("Test three inputs:")
+neuron1 = Neuron(3, 2, 1)
+print("    weighted sum: " + str(neuron1.weightedSum(1, 2, 3)))
+print("    output: " + str(neuron1.output(1, 2, 3)))
 
 
     

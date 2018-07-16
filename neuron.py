74c12538bd4e3dcd5dfdecd6a1d09f93f5c6934c
Eric Laurence <eric@laurences.net>
2018-07-16 10:57:20 -0700

    Add input and coefficient lists

--- neuron.py
+++ neuron.py
@@ -1,7 +1,3 @@
-a = 2
-b = 4
-
-
 class Neuron:
     
     def __init__(self, a, b):
@@ -21,13 +17,31 @@ class Neuron:
     def output (self, x, y):
         return self.activate(self.weightedSum(x, y)) 
 
+input_list = [
+    (5, 3),
+    (7, 0),
+    (3, 5),
+    (-5, 3)
+]
 
-neuron1 = Neuron(a, b)
+coefficient_list = [
+    (2, 4),
+    (9, 3),
+    (5, 2),
+    (8, 5)
+]
 
+neuron_list = []
 
-#print("%d" % weightedSum(5, 3))
-print(neuron1.weightedSum(5, 3)) 
-print(neuron1.activate(neuron1.weightedSum(5, 3)))
-print(neuron1.output(5, 3))
+for (a, b) in coefficient_list:
+    neuron_list.append(Neuron(a, b))
+    
+    
+for neuron_i, neuron in enumerate(neuron_list):
+    print("neuron " + str(neuron_i + 1))
+    for (x, y) in input_list:
+        print("    " + str(neuron.weightedSum(x, y)))
+        print("    " + str(neuron.output(x, y)))
+
 
     
